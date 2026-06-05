4f516d7c5f8d606259cf755b903d576f2b2b6955
prime-fit-club <vahe.khachatryan300@gmail.com>
2026-06-05 14:09:13 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const wishlistButtons =
+document.querySelectorAll('.wishlist');
 
+wishlistButtons.forEach(button=>{
+
+button.addEventListener('click',()=>{
+
+if(button.classList.contains('active')){
+
+button.classList.remove('active');
+button.innerHTML = '♡';
+
+}else{
+
+button.classList.add('active');
+button.innerHTML = '♥';
+
+}
+
+});
+
+});
