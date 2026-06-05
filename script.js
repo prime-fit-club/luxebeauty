21b9e01b8d284b1c7fcb5af42a726e81a52fe6d8
prime-fit-club <vahe.khachatryan300@gmail.com>
2026-06-05 19:25:18 +0400

    Update script.js

--- script.js
+++ script.js
@@ -247,3 +247,40 @@ closeCart.addEventListener('click',()=>{
 cartDrawer.classList.remove('active');
 
 });
+// BACK TO TOP
+
+const backToTop =
+document.querySelector(
+'#backToTop'
+);
+
+window.addEventListener(
+'scroll',
+()=>{
+
+if(window.scrollY > 500){
+
+backToTop.classList.add(
+'show'
+);
+
+}else{
+
+backToTop.classList.remove(
+'show'
+);
+
+}
+
+});
+
+backToTop.addEventListener(
+'click',
+()=>{
+
+window.scrollTo({
+top:0,
+behavior:'smooth'
+});
+
+});
