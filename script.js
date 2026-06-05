4b5f56b2b58643f00c6482add6df1a60af29f920
prime-fit-club <vahe.khachatryan300@gmail.com>
2026-06-05 14:35:45 +0400

    Update script.js

--- script.js
+++ script.js
@@ -20,3 +20,37 @@ button.innerHTML = '♥';
 });
 
 });
+const wishlistButtons =
+document.querySelectorAll('.wishlist');
+
+const wishlistCount =
+document.querySelector('.wishlist-count');
+
+let wishlistNumber = 0;
+
+wishlistButtons.forEach(button=>{
+
+button.addEventListener('click',()=>{
+
+if(button.classList.contains('active')){
+
+button.classList.remove('active');
+button.innerHTML='♡';
+
+wishlistNumber--;
+
+}else{
+
+button.classList.add('active');
+button.innerHTML='♥';
+
+wishlistNumber++;
+
+}
+
+wishlistCount.innerHTML =
+wishlistNumber;
+
+});
+
+});
