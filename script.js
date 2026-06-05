bdba8cb45c4c02990b99c5ad124cc50158745850
prime-fit-club <vahe.khachatryan300@gmail.com>
2026-06-05 16:41:32 +0400

    Update script.js

--- script.js
+++ script.js
@@ -148,3 +148,21 @@ duration:350
 });
 
 });
+// STICKY HEADER EFFECT
+
+const navbar =
+document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+
+if(window.scrollY > 50){
+
+navbar.classList.add('scrolled');
+
+}else{
+
+navbar.classList.remove('scrolled');
+
+}
+
+});
