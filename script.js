3a421074ed5c057206ee1958349ab77d68480b76
prime-fit-club <vahe.khachatryan300@gmail.com>
2026-06-05 20:07:15 +0400

    Update script.js

--- script.js
+++ script.js
@@ -259,3 +259,63 @@ behavior:'smooth'
 });
 
 };
+const glow =
+document.querySelector(
+'.cursor-glow'
+);
+
+document.addEventListener(
+'mousemove',
+(e)=>{
+
+glow.style.left =
+e.clientX + 'px';
+
+glow.style.top =
+e.clientY + 'px';
+
+});
+const cards =
+document.querySelectorAll(
+'.product-card'
+);
+
+cards.forEach(card=>{
+
+card.addEventListener(
+'mousemove',
+(e)=>{
+
+const rect =
+card.getBoundingClientRect();
+
+const x =
+e.clientX - rect.left;
+
+const y =
+e.clientY - rect.top;
+
+const rotateX =
+(y / rect.height - .5) * -12;
+
+const rotateY =
+(x / rect.width - .5) * 12;
+
+card.style.transform =
+`perspective(1000px)
+rotateX(${rotateX}deg)
+rotateY(${rotateY}deg)
+scale(1.04)`;
+
+});
+
+card.addEventListener(
+'mouseleave',
+()=>{
+
+card.style.transform =
+'perspective(1000px) rotateX(0) rotateY(0) scale(1)';
+
+});
+
+});
